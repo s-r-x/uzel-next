0bc1368fcc20f3c5046ceda1440443336ded0db4
srx <strax1990@gmail.com>
2021-03-23 09:51:45 +0500

    little styling for post page

--- screens/post/styled.ts
+++ screens/post/styled.ts
@@ -1,20 +1,30 @@
+import { DevicesConfig } from "@/config/devices";
 import { motion } from "framer-motion";
 import styled from "styled-components";
 
 export const Container = styled.div`
   max-width: 960px;
+  padding: 15px;
   margin: 0 auto;
   img {
     max-width: 100%;
+    height: auto;
   }
   p {
     line-height: 1.45;
+  }
+  figure {
+    margin-left: 0;
+    margin-right: 0;
   }
 `;
 export const Header = styled.header`
   margin-top: 75px;
   margin-bottom: 40px;
   text-align: center;
+  @media (max-width: ${DevicesConfig.mobileBreakpoint}px) {
+    margin-top: 15px;
+  }
 `;
 export const Title = motion.custom(styled.h1`
   font-weight: 900;
@@ -23,6 +33,9 @@ export const Title = motion.custom(styled.h1`
   font-family: var(--font-secondary);
   perspective: 2000;
   margin-bottom: 3px;
+  @media (max-width: ${DevicesConfig.mobileBreakpoint}px) {
+    font-size: 32px;
+  }
 `);
 export const Date = styled.time`
   font-weight: 400;
