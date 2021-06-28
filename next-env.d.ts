0d4eaf26073e4c91e423ca2fb899146f57970da2
s-r-x <strax1990@gmail.com>
2021-06-28 09:55:51 +0500

    bump next@11

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,20 +1,3 @@
-import { CSSProp } from "styled-components";
-
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-
-declare module "react" {
-  interface Attributes {
-    css?: CSSProp;
-  }
-}
-declare module "scroll-lock" {
-  export function disablePageScroll(): void;
-  export function enablePageScroll(): void;
-}
-export type TStringDict = {
-  [key: string]: string;
-};
-export type TAnyDict = {
-  [key: string]: any;
-};
+/// <reference types="next/image-types/global" />
